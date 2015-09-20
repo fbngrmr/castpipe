d301327c14ad7947022d11a313a222e5cff53d8f
fbngrmr <fbngrmr@mailbox.org>
2015-09-20 22:59:38 +0200

    Make host, port configurable

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 'use strict';
 
-var serveStdIn = require('./lib/server.js');
+var serveStdIn = require('./lib/serveStdIn.js');
+var launchChromecastStreamingApp = require('./lib/sendChromecast.js');
+var host = 'localhost';
+var port = 1337;
 
 serveStdIn({
-  host: 'localhost',
-  port:  1337
+  host: host,
+  port:  host
+});
+
+launchChromecastStreamingApp({
+  title: 'Pipecast',
+  image: '/tmp/foo.png',
+  host: host,
+  port: port
 });
 
