2d3e53148913f279601d142590d9f0aab8fca668
fbngrmr <fbngrmr@mailbox.org>
2015-09-21 14:15:44 +0200

    Refactoring and fixing stupid mistakes

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+#!/usr/bin/env node
+
 'use strict';
 
 var serveStdIn = require('./lib/serveStdIn.js');
 var launchChromecastStreamingApp = require('./lib/sendChromecast.js');
-var host = 'localhost';
-var port = 1337;
+var argv = require('yargs')
+    .usage('Usage: $0 --localIP --port [num]')
+    .demand(['localIP'])
+    .argv;
 
-serveStdIn({
-  host: host,
-  port:  host
-});
+var server = argv.localIP;
+var port = argv.port || 1338;
 
 launchChromecastStreamingApp({
   title: 'Pipecast',
-  image: '/tmp/foo.png',
-  host: host,
+  server: server,
   port: port
+}, function startServer() {
+  serveStdIn({
+    listenOn: server,
+    port: port
+  });
 });
-
