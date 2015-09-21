86111a48015b5ce98fb5bdce0666747b42a75e0a
fbngrmr <fbngrmr@mailbox.org>
2015-09-21 14:51:07 +0200

    Better CLI option parsing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #pipecast
 
-Pipe video content to a Chromecast receiver. Pre-alpha.
+Pipe video content to a Chromecast receiver. Alpha.
 
 ## Installation
 ```
@@ -10,5 +10,12 @@ npm install
 
 ## Usage
 ```
-cat clip.avi | ffmpeg -i - -strict experimental -f matroska -vcodec h264 -acodec aac -ac 2 pipe:1 -profile:v high -level 4.2
+cat clip.avi | ffmpeg -i - -strict experimental -f matroska -vcodec h264 -acodec aac -ac 2 pipe:1 -profile:v high -level 4.2 | node index.js --localIP=192.168.1.69
 ```
+
+### Options
+* `--localIP={IP}` Set this to local IP address of your computer
+* `--port={PORT}` Set this to the port you want the server to listen on
+
+## License
+Licensed under the MIT license.
--- index.js
+++ index.js
@@ -5,14 +5,15 @@
 var serveStdIn = require('./lib/serveStdIn.js');
 var launchChromecastStreamingApp = require('./lib/sendChromecast.js');
 var argv = require('yargs')
-    .usage('Usage: $0 --localIP --port [num]')
-    .demand(['localIP'])
+    .usage('Usage: $0 --localIP [--port]')
+    .option( "localIP", {demand: true, describe: "IP address of computer", type: "string" } )
+    .option( "port", { demand: false, describe: "Port for server to listen on" } )
     .argv;
 
 var server = argv.localIP;
 var port = argv.port || 1338;
 
-launchChromecastStreamingApp({
+/*launchChromecastStreamingApp({
   title: 'Pipecast',
   server: server,
   port: port
@@ -21,4 +22,4 @@ launchChromecastStreamingApp({
     listenOn: server,
     port: port
   });
-});
+});*/
